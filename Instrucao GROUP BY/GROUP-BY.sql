770b0a52d3a58c4d3ad2dc2aca7720f424ad6791
giovanna midonz <gmidonz09@outlook.com>
2026-03-12 12:39:45 -0300

    ORDERBY-SUM-ROUND-AVG

--- Instrucao GROUP BY/GROUP-BY.sql
+++ Instrucao GROUP BY/GROUP-BY.sql
@@ -1,5 +1,6 @@
 SELECT rating, AVG(rental_rate)
-FROM film WHERE rating  IN ('PG', 'R','G')
+FROM film 
+WHERE rating  IN ('PG', 'R','G')
 GROUP BY rating ;
 
 SELECT rating, COUNT(*)
@@ -14,3 +15,22 @@ SELECT staff_id, SUM(amount)
 FROM payment
 GROUP BY staff_id;
 
+SELECT staff_id, COUNT(staff_id)
+FROM payment 
+GROUP BY staff_id;
+
+SELECT * FROM film;
+
+
+SELECT rating, ROUND(AVG(replacement_cost),2)
+FROM film 
+GROUP BY rating; 
+
+
+SELECT * FROM payment;
+
+SELECT customer_id, SUM(amount)
+from payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
