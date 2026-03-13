59419388970712a727f74e0b3163f3bdffa8d6ce
giovanna midonz <gmidonz09@outlook.com>
2026-03-13 12:49:59 -0300

    HAVING

---
+++ Instrucao GROUP BY/HAVING.sql
@@ -0,0 +1,13 @@
+SELECT * FROM payment;
+
+SELECT customer_id, COUNT(*)
+FROM payment
+GROUP BY customer_id
+HAVING COUNT(*) >= 40;
+
+SELECT customer_id, SUM(amount) 
+FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) >= 100;
+
